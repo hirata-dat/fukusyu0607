b598ad4e03e84e0e14047ae1ab549bd5915ca8bf
hirata-dat <49702207+hirata-dat@users.noreply.github.com>
2019-06-07 14:30:11 +0900

    乱数１

--- fukusyu0607/Form1.cs
+++ fukusyu0607/Form1.cs
@@ -12,12 +12,26 @@ namespace fukusyu0607
 {
     public partial class Form1 : Form
     {
-        int vx = -10;
-        int vy = -10;
+        //疑似乱数
+        //ランダムのシード（種）を指定して初期化したら
+        //それを使い続ける
+        private static Random rand = new Random();
         
+        int vx = rand.Next(-50,20);
+        int vy = rand.Next(-50,20);
+        
+        
+
         public Form1()
         {
             InitializeComponent();
+
+            //以下に、label1.leftとlabel1.Topの座標をランダムで求めよ
+
+
+
+
+
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -27,17 +41,31 @@ namespace fukusyu0607
             //Width =幅（横）
             //Height =高さ（縦）
             //ClientSize =（フォーム）
-        }
+        }//マウスの（X.Y）がわかる
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             Text = MousePosition.X + "," + MousePosition.Y;
-            Point p = PointToClient(MousePosition);
+           // Point p = PointToClient(MousePosition);
 
             label1.Left += vx;//ラベルが左に行く
             label1.Top  += vy;//ラベルが上に行く
             //結果：左上に行く
-            
+
+            //マウスと重なった時、タイマーが止める
+            Point p = PointToClient(MousePosition);
+            if(     (p.X>=label1.Left)
+                &&  (p.X< label1.Right)
+                &&  (p.Y>=label1.Top)
+                &&  (p.Y<label1.Bottom)
+               )
+
+            {
+                timer1.Enabled = false;
+            }
+
+
+
             if(label1.Left<0)
             {
                 vx = Math.Abs(vx);//左に来たら跳ね返す
@@ -50,13 +78,17 @@ namespace fukusyu0607
             {
                 vx = -vx;
             }
-            if(label1.Bottom>500)
+            if (label1.Bottom > 500)//下に来たら跳ね返す
             {
-                vy = -vy;//下に来たら跳ね返す
+                vy = -vy;
             }
             if(label1.Left>ClientSize.Width-label1.Width)
             {
-                vx = -Math.Abs(vx);//画面に合わせて跳ね返す
+                vx = -Math.Abs(vx);//画面に合わせて跳ね返す(左)
+            }
+            if(label1.Right>ClientSize.Width-label1.Width)
+            {
+                vx = -Math.Abs(vx);//画面に合わせて跳ね返す(右)
             }
 
 
@@ -64,8 +96,7 @@ namespace fukusyu0607
 
 
 
-
-        }
+        }//画面上で跳ね返す
 
         private void Form1_Load(object sender, EventArgs e)
         {
